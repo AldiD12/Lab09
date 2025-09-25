a7cee912b6ac26bc801e59d2beaa0d83bd5de14c
AldiD <134085752+AldiD12@users.noreply.github.com>
2025-09-25 12:31:15 +0200

    Update B_while_pin.py

--- B_while_pin.py
+++ B_while_pin.py
@@ -3,7 +3,7 @@ perpjekje_te_mbetura = 3
 hyrje_e_lejuar = False
 
 while perpjekje_te_mbetura > 0:
-    pin_i_dhene = input(f"Jepni PIN-in ({perpjekje_te_mbetura} përpjekje të mbetura): ")
+    pin_i_dhene = input(f"Jepni PIN-in ({perpjekje_te_mbetura} perpjekje te mbetura): ")
     if pin_i_dhene == PIN_KORREKT:
         print("Hyrje e lejuar")
         hyrje_e_lejuar = True
@@ -11,7 +11,7 @@ while perpjekje_te_mbetura > 0:
     else:
         perpjekje_te_mbetura -= 1
         if perpjekje_te_mbetura > 0:
-            print(f"PIN i gabuar. Ju kanë mbetur {perpjekje_te_mbetura} përpjekje.")
+            print(f"PIN i gabuar. Ju kane mbetur {perpjekje_te_mbetura} perpjekje.")
 
 if not hyrje_e_lejuar:
     print("Karta u bllokua")
@@ -19,14 +19,14 @@ else:
 
     while True:
         try:
-            n_str = input("\nJepni një numër të plotë jo-negativ (n >= 0): ")
+            n_str = input("\nJepni nje numer te plote jo-negativ (n >= 0): ")
             n = int(n_str)
             if n >= 0:
                 break
             else:
-                print("Ju lutem jepni një numër jo-negativ.")
+                print("Ju lutem jepni nje numer jo-negativ.")
         except ValueError:
-            print("Ju lutem jepni një numër të vlefshëm të plotë.")
+            print("Ju lutem jepni nje numer te vlefshem te plote.")
 
     shuma = 0
     faktoriali = 1
@@ -35,6 +35,7 @@ else:
         shuma += i
         faktoriali *= i
 
-    print(f"\nRezultatet për n = {n}:")
-    print(f"Shuma (S = 1 + 2 + ... + {n}) është: {shuma}")
-    print(f"Faktoriali ({n}!) është: {faktoriali}")
+    print(f"\nRezultatet per n = {n}:")
+    print(f"Shuma (S = 1 + 2 + ... + {n}) eshte: {shuma}")
+    print(f"Faktoriali ({n}!) eshte: {faktoriali}")
+
