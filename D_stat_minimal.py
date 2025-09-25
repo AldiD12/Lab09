984f02d4d8a3629532b0630145f6a81375e26c90
AldiD <134085752+AldiD12@users.noreply.github.com>
2025-09-25 12:31:26 +0200

    Update D_stat_minimal.py

--- D_stat_minimal.py
+++ D_stat_minimal.py
@@ -3,7 +3,7 @@ total_sum = 0.0
 min_val = None
 max_val = None
 
-print("Jepni numra një nga një. Shkruani 'stop' për të përfunduar.")
+print("Jepni numra nje nga nje. Shkruani 'stop' per te perfunduar.")
 
 while True:
     txt = input("> ")
@@ -14,7 +14,7 @@ while True:
     try:
         num = float(txt)
     except ValueError:
-        print("Vlerë e pavlefshme")
+        print("Vlere e pavlefshme")
         continue  
     count += 1
     total_sum += num
@@ -24,12 +24,13 @@ while True:
     if max_val is None or num > max_val:
         max_val = num
 
-print("\n--- Përmbledhja Statistikore ---")
+print("\n--- Permbledhja Statistikore ---")
 if count == 0:
-    print("Nuk u dhanë të dhëna.")
+    print("Nuk u dhane te dhena.")
 else:
     mesatarja = total_sum / count
     print(f"Numri i vlerave (Count): {count}")
     print(f"Vlera Minimale (Min): {min_val}")
     print(f"Vlera Maksimale (Max): {max_val}")
     print(f"Mesatarja: {mesatarja:.2f}")
+
