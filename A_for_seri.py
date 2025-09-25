2941b104c6836dbba3b448b9a1ac9bfb5decf5ce
AldiD <134085752+AldiD12@users.noreply.github.com>
2025-09-25 12:30:59 +0200

    Update A_for_seri.py

--- A_for_seri.py
+++ A_for_seri.py
@@ -1,13 +1,13 @@
 while True:
     try:
-        n_str = input("Jepni një numër të plotë jo-negativ (n >= 0): ")
+        n_str = input("Jepni nje numer te plote jo-negativ (n >= 0): ")
         n = int(n_str)
         if n >= 0:
             break
         else:
-            print("Ju lutem jepni një numër jo-negativ.")
+            print("Ju lutem jepni nje numer jo-negativ.")
     except ValueError:
-        print("Ju lutem jepni një numër të vlefshëm të plotë.")
+        print("Ju lutem jepni nje numer te vlefshem te plote.")
 
 shuma = 0
 faktoriali = 1
@@ -16,6 +16,7 @@ for i in range(1, n + 1):
     shuma += i
     faktoriali *= i
 
-print(f"\nRezultatet për n = {n}:")
-print(f"Shuma (S = 1 + 2 + ... + {n}) është: {shuma}")
-print(f"Faktoriali ({n}!) është: {faktoriali}")
+print(f"\nRezultatet per n = {n}:")
+print(f"Shuma (S = 1 + 2 + ... + {n}) eshte: {shuma}")
+print(f"Faktoriali ({n}!) eshte: {faktoriali}")
+
